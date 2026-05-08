cdbd62b3c57178671ea442629946328d65ff25bd
AndreAugustoSilva <andre_augusto-silva@estudante.sesisenai.org.br>
2026-05-08 20:48:11 -0300

    Questão4SQL

---
+++ cinemaQuestão4.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE CINEMA;
+USE CINEMA;
+
+CREATE TABLE CLIENTE (
+	id_usuario int primary key auto_increment,
+    nome varchar(200) not null,
+    maior_idade boolean not null,
+    rg char(9)
+);
+CREATE TABLE PEDIDO (
+	id_pedido int primary key auto_increment,
+    forma_pagamento varchar(100)
+);
+CREATE TABLE INGRESSO (
+	id_ingresso int primary key auto_increment,
+    categoria varchar(50)
+);
+CREATE TABLE SESSAO (
+	id_sessao int primary key auto_increment,
+    horario date
+);
+CREATE TABLE SALA (
+	id_sala int primary key auto_increment,
+    vip boolean,
+    threed boolean
+);
+CREATE TABLE FILME (
+	id_filme int primary key auto_increment,
+    faixa_etaria char(3),
+    duracao date,
+    classificacao varchar(50)
+);
+CREATE TABLE VENDA (
+	id_venda int primary key auto_increment,
+    id_ingresso int,
+    id_pedido int,
+    foreign key(id_ingresso) references INGRESSO(id_ingresso),
+    foreign key(id_pedido) references PEDIDO(id_pedido)
+);
+
+select * from CLIENTE;
+select * from PEDIDO;
+select * from INGRESSO;
+select * from SESSAO;
+select * from SALA;
+select * from FILME;
+select * from VENDA;
